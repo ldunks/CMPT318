bca965eab1ba74213b547f92b2dbf8cbdae715da
michellepark53 <michellehyunjipark@gmail.com>
2024-09-22 21:55:54 -0700

    solve Q2

--- A1.R
+++ A1.R
@@ -1,4 +1,4 @@
-
+#Q1
 # installed zoo to have access to the approx function to do linear interpolation
 install.packages("zoo")
 library(zoo)
@@ -65,6 +65,29 @@ filtered_data <- DataDf %>%
 
 
 ####### Q2 uses filtered_data #######
+#Q2
+library(corrplot)
+
+columns <- c("Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", 
+             "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+#loop to calculate correlations
+for (i in 1:length(columns)) {
+  for (j in (i+1):length(columns)) {
+    var1 <- filtered_data[[columns[i]]]
+    var2 <- filtered_data[[columns[j]]]
+    
+    correlation <- cor(var1, var2, method = "pearson")
+    
+    print(sprintf("Correlation between %s and %s: %0.2f", columns[i], columns[j], correlation))
+  }
+}
+
+#http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
+#used "color" and "upper" styles to color code and display upper triangular of correlation matrix 
+corrplot(cor_matrix, method = "color", type = "upper", 
+         tl.col = "black", tl.srt = 45, tl.cex = 0.6, addCoef.col = "black", 
+         number.cex = 0.6, col = colorRampPalette(c("blue", "white", "red"))(200))
 
 
 
